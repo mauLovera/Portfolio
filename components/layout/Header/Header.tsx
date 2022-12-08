5c2bc752c334a0c0ea1b6d4055ebc754eb919ff7
mauLovera <mauricioloveraedu@gmail.com>
2022-12-08 18:49:53 -0500

    feat: add tsx for header component && react-icons

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,5 +1,26 @@
+import Link from 'next/link'
+import ActiveLink from '@/components/common/ActiveLink/ActiveLink'
 import styles from './Header.module.scss'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Header() {
-  return <header className={styles.container}>Header</header>
+  return (
+    <header className={styles.container}>
+      <nav>
+        <Link href={'/'} className={styles.name}>
+          Mauricio Lovera
+        </Link>
+        <div className={styles.title}>
+          <span>Full Stack Web Developer</span>
+          <FaGithub />
+          <FaLinkedin />
+        </div>
+        <div className={styles.links}>
+          <ActiveLink text="Projects" url="projects" />
+          <ActiveLink text="About" url="about" />
+          <ActiveLink text="Contact" url="contact" />
+        </div>
+      </nav>
+    </header>
+  )
 }
