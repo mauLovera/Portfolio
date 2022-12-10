af9b222c1f1f307031f1296decdafd5811ffaa7d
mauLovera <mauricioloveraedu@gmail.com>
2022-12-10 10:41:59 -0500

    refactor: extract variants into variants.ts file

--- components/common/SelectedProjects/SelectedProjects.tsx
+++ components/common/SelectedProjects/SelectedProjects.tsx
@@ -1,68 +1,25 @@
-import SectionSubHeader from './SectionSubHeader/SectionSubHeader'
-import SectionHeader from './SectionHeader/SectionHeader'
-import { RiArrowRightCircleFill } from 'react-icons/ri'
-import styles from './SelectedProjects.module.scss'
-import { animate, motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import Row from './Row/Row'
 import { useState } from 'react'
+import { animate, motion } from 'framer-motion'
+import { RiArrowRightCircleFill } from 'react-icons/ri'
+import styles from './SelectedProjects.module.scss'
+
+//* === Components === */
+import SectionSubHeader from './SectionSubHeader/SectionSubHeader'
+import SectionHeader from './SectionHeader/SectionHeader'
 import List from './List/List'
 import Slides from './Slides/Slides'
 
 //* === Variants === //
+import {
+  timeline,
+  buttonAnimation,
+  letterAnimation,
+  letterTimeline,
+  lineAnimation,
+} from './variants'
 
-const timeline = {
-  animate: {
-    transition: {
-      delayChildren: 0,
-      staggerChildren: 0.5,
-    },
-  },
-}
-
-const letterTimeline = {
-  animate: {
-    transition: {
-      delayChildren: 0.4,
-      staggerChildren: 0.025,
-    },
-  },
-}
-
-const lineAnimation = {
-  initial: { y: 200 },
-  animate: {
-    y: 0,
-    transition: {
-      ease: [0.6, 0.01, -0.05, 0.95],
-      duration: 1,
-    },
-  },
-}
-
-const letterAnimation = {
-  initial: { y: 400 },
-  animate: {
-    y: 0,
-    transition: {
-      ease: [0.6, 0.01, -0.05, 0.95],
-      duration: 1,
-    },
-  },
-}
-
-const buttonAnimation = {
-  initial: { scale: 0 },
-  animate: {
-    scale: 1,
-    transition: {
-      ease: [0.6, 0.01, -0.05, 0.95],
-      duration: 1,
-      delay: 0.5,
-    },
-  },
-}
 
 export default function SelectedProjects() {
   const [focus, setFocus] = useState<string>('')
