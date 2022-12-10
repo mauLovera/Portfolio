cb9c4e6b42763f41013a8ed1c8381755d92f1f2c
mauLovera <mauricioloveraedu@gmail.com>
2022-12-09 19:01:03 -0500

    feat: add counter to row component

--- components/common/SelectedProjects/Row/Row.tsx
+++ components/common/SelectedProjects/Row/Row.tsx
@@ -1,6 +1,7 @@
-import { motion, Variants } from 'framer-motion'
 import Link from 'next/link'
 import styles from './Row.module.scss'
+import { motion, Variants } from 'framer-motion'
+import { RiArrowRightCircleFill } from 'react-icons/ri'
 
 interface Props {
   title: string
@@ -16,16 +17,30 @@ export default function Row({ title, path, timeline, animation }: Props) {
   return (
     <Link href={path} className={styles.container}>
       <motion.span
-        className={styles.title}
+        className={styles.titleContainer}
         variants={timeline}
         initial="initial"
         whileInView={'animate'}
       >
-        <motion.span variants={animation.lineAnimation}>{title}</motion.span>
+        <motion.span
+          variants={animation.lineAnimation}
+          className={styles.title}
+        >
+          {title}
+        </motion.span>
+      </motion.span>
+      <motion.span
+        variants={timeline}
+        initial="initial"
+        whileInView={'animate'}
+        className={styles.button}
+      >
         <motion.span
           variants={animation.buttonAnimation}
           className={styles.arrow}
-        ></motion.span>
+        >
+          <RiArrowRightCircleFill />
+        </motion.span>
       </motion.span>
     </Link>
   )
