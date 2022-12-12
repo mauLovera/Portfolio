e82b911bc8ba3908b55f995e429382eab6edd31d
mauLovera <mauricioloveraedu@gmail.com>
2022-12-12 09:28:54 -0500

    feat: add project number to description component

--- components/projects/Display/Description/Description.tsx
+++ components/projects/Display/Description/Description.tsx
@@ -2,6 +2,11 @@ import styles from './Description.module.scss'
 
 export default function Description() {
   return (
-    <div className={styles.container}>Description</div>
+    <div className={styles.container}>
+      <div className={styles.projectNumber}>
+        <span className={styles.ascendingNumber}></span>
+        <span>/00-4</span>
+      </div>
+    </div>
   )
 }
