44a8618e0e567272f7eccfc096a70694a2280b0e
mauLovera <mauricioloveraedu@gmail.com>
2022-12-13 12:57:42 -0500

    feat: add footer component

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,5 +1,14 @@
+import ActiveLink from '@/components/common/ActiveLink/ActiveLink'
+import Header from '../Header/Header'
 import styles from './Footer.module.scss'
 
 export default function Footer() {
-  return <footer className={styles.container}>Footer</footer>
+  return (
+    <footer className={styles.container}>
+      <ActiveLink text="Home" url="" />
+      <ActiveLink text="Projects" url="projects" ammount='4'/>
+      <ActiveLink text="About" url="about" />
+      <ActiveLink text="Contact" url="contact" />
+    </footer>
+  )
 }
